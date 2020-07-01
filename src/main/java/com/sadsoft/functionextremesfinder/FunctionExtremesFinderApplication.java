7ab653f8c1f3f415f55fd4d131448c1c0f338c3a
bulzaj <ernestsado94@gmail.com>
2020-07-01 18:06:45 +0200

    first commit

---
+++ src/main/java/com/sadsoft/functionextremesfinder/FunctionExtremesFinderApplication.java
@@ -0,0 +1,13 @@
+package com.sadsoft.functionextremesfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionExtremesFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunctionExtremesFinderApplication.class, args);
+    }
+
+}
