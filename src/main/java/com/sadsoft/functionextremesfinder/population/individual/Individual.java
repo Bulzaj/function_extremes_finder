6ea20f3da614b2d85dd178ac7ade88ad24f049d7
bulzaj <ernestsado94@gmail.com>
2020-07-03 01:09:41 +0200

    Added fitness field in Individual

--- src/main/java/com/sadsoft/functionextremesfinder/population/individual/Individual.java
+++ src/main/java/com/sadsoft/functionextremesfinder/population/individual/Individual.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public interface Individual {
 
-    double getFitness();
+    double getValue();
     int[] getGenes();
+    double getFitness();
 }
--- src/main/java/com/sadsoft/functionextremesfinder/population/individual/IndividualImpl.java
+++ src/main/java/com/sadsoft/functionextremesfinder/population/individual/IndividualImpl.java
@@ -1,17 +1,20 @@
 package com.sadsoft.functionextremesfinder.population.individual;
 
+import java.util.Arrays;
+
 public class IndividualImpl implements Individual {
 
     private int[] genes;
+    double fitness;
 
     public IndividualImpl(int[] genes) {
         validateGenes(genes);
         this.genes = genes;
-
+        fitness = 0;
     }
 
     @Override
-    public double getFitness() {
+    public double getValue() {
         double result = 0;
         int j=0;
         for (int i=genes.length-1; i>=0; i--) {
@@ -21,9 +24,22 @@ public class IndividualImpl implements Individual {
         return result;
     }
 
+    public void setGenes(int[] genes) {
+        this.genes = genes;
+    }
+
     @Override
     public int[] getGenes() {
         return genes;
+    }
+
+    public void setFitness(double fitness) {
+        this.fitness = fitness;
+    }
+
+    @Override
+    public double getFitness() {
+        return fitness;
     }
 
     private void validateGenes(int[] genes) {
@@ -33,5 +49,10 @@ public class IndividualImpl implements Individual {
                 throw new IllegalArgumentException("Genes can have values 1 or 0");
             }
         }
+    }
+
+    @Override
+    public String toString() {
+        return "Genes: " + Arrays.toString(genes) + " Value: " + getValue();
     }
 }
