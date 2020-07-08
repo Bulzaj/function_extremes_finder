daa934caf64731e9a2a00e3b7b2bf29e76cb8929
bulzaj <ernestsado94@gmail.com>
2020-07-08 22:35:24 +0200

    initialized response dto

--- src/main/java/com/sadsoft/functionextremesfinder/model/GeneticAlgorithmResponseDTO.java
+++ src/main/java/com/sadsoft/functionextremesfinder/model/GeneticAlgorithmResponseDTO.java
@@ -2,4 +2,30 @@ package com.sadsoft.functionextremesfinder.model;
 
 public class GeneticAlgorithmResponseDTO {
 
+    private int x;
+    private double xValue;
+
+    public GeneticAlgorithmResponseDTO(int x, double xValue) {
+        this.x = x;
+        this.xValue = xValue;
+    }
+
+    public GeneticAlgorithmResponseDTO() {
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public double getxValue() {
+        return xValue;
+    }
+
+    public void setxValue(double xValue) {
+        this.xValue = xValue;
+    }
 }
