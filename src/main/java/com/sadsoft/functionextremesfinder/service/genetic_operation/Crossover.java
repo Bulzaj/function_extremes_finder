4faab81525d21622542ac74b3565f6150c745822
bulzaj <ernestsado94@gmail.com>
2020-07-11 20:18:59 +0200

    added Crossover feature

---
+++ src/main/java/com/sadsoft/functionextremesfinder/service/genetic_operation/Crossover.java
@@ -0,0 +1,11 @@
+package com.sadsoft.functionextremesfinder.service.genetic_operation;
+
+import com.sadsoft.functionextremesfinder.model.Population;
+
+public class Crossover implements GeneticOperator {
+
+    @Override
+    public void doOperation(Population population) {
+
+    }
+}
