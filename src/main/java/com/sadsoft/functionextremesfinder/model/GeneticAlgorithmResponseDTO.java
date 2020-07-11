e724eb62d2d776f56c7e192f94b166fff169063c
bulzaj <ernestsado94@gmail.com>
2020-07-11 19:59:52 +0200

    improved response entity

--- src/main/java/com/sadsoft/functionextremesfinder/model/GeneticAlgorithmResponseDTO.java
+++ src/main/java/com/sadsoft/functionextremesfinder/model/GeneticAlgorithmResponseDTO.java
@@ -4,13 +4,14 @@ public class GeneticAlgorithmResponseDTO {
 
     private int x;
     private double xValue;
+    private int iterations;
+    private StopReason stopReason;
 
-    public GeneticAlgorithmResponseDTO(int x, double xValue) {
+    public GeneticAlgorithmResponseDTO(int x, double xValue, int iterations, StopReason stopReason) {
         this.x = x;
         this.xValue = xValue;
-    }
-
-    public GeneticAlgorithmResponseDTO() {
+        this.iterations = iterations;
+        this.stopReason = stopReason;
     }
 
     public int getX() {
@@ -27,5 +28,21 @@ public class GeneticAlgorithmResponseDTO {
 
     public void setxValue(double xValue) {
         this.xValue = xValue;
+    }
+
+    public int getIterations() {
+        return iterations;
+    }
+
+    public void setIterations(int iterations) {
+        this.iterations = iterations;
+    }
+
+    public StopReason getStopReason() {
+        return stopReason;
+    }
+
+    public void setStopReason(StopReason stopReason) {
+        this.stopReason = stopReason;
     }
 }
