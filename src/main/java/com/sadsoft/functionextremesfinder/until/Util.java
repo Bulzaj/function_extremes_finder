ca784adf5e67af0ec657d267cbeac53ac189c8ae
bulzaj <ernestsado94@gmail.com>
2020-07-11 20:02:41 +0200

    added new util features

--- src/main/java/com/sadsoft/functionextremesfinder/until/Util.java
+++ src/main/java/com/sadsoft/functionextremesfinder/until/Util.java
@@ -1,7 +1,9 @@
 package com.sadsoft.functionextremesfinder.until;
 
+import com.sadsoft.functionextremesfinder.model.Population;
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
+import org.slf4j.Logger;
 
 public class Util {
 
@@ -9,12 +11,21 @@ public class Util {
         return (int) ((Math.random() * (max+1 - min)) + min);
     }
 
-    public static double computeFunctionValueInX(double x, String functionBody) {
+    public static float generateRandom(float min, float max) {
+        return (float) (min + Math.random() * (max - min));
+    }
+
+    public static float computeFunctionValueInX(double x, String functionBody) {
         String newFunctionBody = functionBody.replace("ln", "2.303*log10");
         Expression expression = new ExpressionBuilder(newFunctionBody)
                 .variable("x")
                 .build()
                 .setVariable("x", x);
-        return expression.evaluate();
+        return (float) expression.evaluate();
+    }
+
+    public static void logPopulation(Population population, Logger log) {
+        log.info("Population:");
+        population.getPopulation().forEach(individual -> log.info(individual.toString()));
     }
 }
