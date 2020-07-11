fa8491466e2b01c627b05ea10f5c3cfab8bdb341
bulzaj <ernestsado94@gmail.com>
2020-07-11 20:04:44 +0200

    changed http method

--- src/main/java/com/sadsoft/functionextremesfinder/api/GeneticAlgorithmController.java
+++ src/main/java/com/sadsoft/functionextremesfinder/api/GeneticAlgorithmController.java
@@ -2,12 +2,10 @@ package com.sadsoft.functionextremesfinder.api;
 
 import com.sadsoft.functionextremesfinder.model.GeneticAlgorithmPropertiesRequestDTO;
 import com.sadsoft.functionextremesfinder.model.GeneticAlgorithmResponseDTO;
-import com.sadsoft.functionextremesfinder.service.GeneticAlgorithmService;
+import com.sadsoft.functionextremesfinder.service.genetic_algorithm.GeneticAlgorithmService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
