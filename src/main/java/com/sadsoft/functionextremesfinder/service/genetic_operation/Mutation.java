8a5d0c50155f3b6dd768c0def3b93c1d2da93f0c
bulzaj <ernestsado94@gmail.com>
2020-07-14 18:45:46 +0200

    implemented new mutation method

--- src/main/java/com/sadsoft/functionextremesfinder/service/genetic_operation/Mutation.java
+++ src/main/java/com/sadsoft/functionextremesfinder/service/genetic_operation/Mutation.java
@@ -19,28 +19,48 @@ public class Mutation implements GeneticOperator {
 
     @Override
     public void doOperation(Population population, GeneticAlgorithmPropertiesRequestDTO request) {
-        log.debug("[Mutation] Mutation starts");
-        Util.logPopulation(population, log);
-        population
-                .getPopulation()
-                .stream()
-                .filter(individual -> {
-                    int random = Util.generateRandom(0, 100);
-                    if (random <= request.getMutationChance()) return true;
-                    return false;
-                })
-                .map(individual -> {
-                    int locus = Util.generateRandom(0, individual.getGenes().length-1);
-                    int newValue = 1-individual.getGenes()[locus];
-                    Individual newIndividual = new Individual();
-                    int[] newGenes = individual.getGenes();
-                    newGenes[locus] = newValue;
-                    newIndividual.setGenes(individual.getGenes());
-                    return newIndividual;
-                })
-                .collect(Collectors.toList());
+
+        log.debug("[Mutation] Mutation starts...");
+
+        for (int i=0; i<population.getPopulation().length; i++) {
+            int mutationChance = Util.generateRandom(0, 100);
+            if (mutationChance <= request.getMutationChance()) {
+                int locus = Util.generateRandom(0, Util.getRangeLength(request.getMaxRange())-1);
+                int newValue = 1-population.getPopulation()[i].getGenes()[locus];
+                population.getPopulation()[i].getGenes()[locus] = newValue;
+                log.debug("[Mutation] Mutation occurs for individual {}, locus={}",
+                        i,
+                        locus);
+            }
+        }
         Util.updatePopulation(population, request);
-        Util.logPopulation(population, log);
-        log.debug("[Mutation] Mutation stop...");
+        log.debug("[Mutation] Mutation stops...");
     }
+
+//    @Override
+//    public void doOperation(Population population, GeneticAlgorithmPropertiesRequestDTO request) {
+//        log.debug("[Mutation] Mutation starts");
+//        Util.logPopulation(population, log);
+//        population
+//                .getPopulation()
+//                .stream()
+//                .filter(individual -> {
+//                    int random = Util.generateRandom(0, 100);
+//                    if (random <= request.getMutationChance()) return true;
+//                    return false;
+//                })
+//                .map(individual -> {
+//                    int locus = Util.generateRandom(0, individual.getGenes().length-1);
+//                    int newValue = 1-individual.getGenes()[locus];
+//                    Individual newIndividual = new Individual();
+//                    int[] newGenes = individual.getGenes();
+//                    newGenes[locus] = newValue;
+//                    newIndividual.setGenes(individual.getGenes());
+//                    return newIndividual;
+//                })
+//                .collect(Collectors.toList());
+//        Util.updatePopulation(population, request);
+//        Util.logPopulation(population, log);
+//        log.debug("[Mutation] Mutation stop...");
+//    }
 }
