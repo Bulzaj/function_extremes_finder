bbcff4627bba6210abbcf30f1cb0dbe5078d114f
bulzaj <ernestsado94@gmail.com>
2020-07-14 16:08:30 +0200

    cleanup

--- src/main/java/com/sadsoft/functionextremesfinder/model/Individual.java
+++ src/main/java/com/sadsoft/functionextremesfinder/model/Individual.java
@@ -45,26 +45,4 @@ public class Individual  {
     public String toString() {
         return "Genes: " + Arrays.toString(genes) + " x: " + getX() + " Value: " + getValue() + " Fitness: " + getFitness();
     }
-
-    private void validateGenes(int[] genes) {
-        if (genes == null) throw new NullPointerException("Genes array can't be null");
-        for(int gene : genes) {
-            if (gene != 1 && gene != 0) {
-                throw new IllegalArgumentException("Genes can have values 1 or 0");
-            }
-        }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Individual that = (Individual) o;
-        return Arrays.equals(genes, that.genes);
-    }
-
-    @Override
-    public int hashCode() {
-        return Arrays.hashCode(genes);
-    }
 }
